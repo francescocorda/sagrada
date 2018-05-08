3751302e578290bdb4b21a93f4a9cf727754d622
francescocorda <francesco.corda@mail.polimi.it>
2018-05-08 16:51:19 +0200

    Modified pom.xml

--- src/test/java/it/polimi/ingsw/TestPrivateObjectiveCard.java
+++ src/test/java/it/polimi/ingsw/TestPrivateObjectiveCard.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class iTestPrivateObjectiveCard  {
+class TestPrivateObjectiveCard  {
 
     @Test
     void countScoreTest() {

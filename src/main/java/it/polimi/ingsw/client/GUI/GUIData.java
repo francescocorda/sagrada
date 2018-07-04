ee7a4e4ed7d1213cae078e17eff26e20831d8285
federicocomolli <federico1.comolli@mail.polimi.it>
2018-07-04 23:54:02 +0200

    Reduced quality of loginBackground.jpg image.

--- src/main/java/it/polimi/ingsw/client/GUI/GUIData.java
+++ src/main/java/it/polimi/ingsw/client/GUI/GUIData.java
@@ -6,11 +6,6 @@ import javafx.event.ActionEvent;
 import javafx.stage.Stage;
 
 public class GUIData {
-    private ActionEvent event;
-    private Stage stage;
-    private Communicator communicator;
-    private String username;
-    private GUIView view;
     private static GUIData instance= null;
     public static synchronized GUIData getGUIData() {
         if (instance == null) {
@@ -18,37 +13,78 @@ public class GUIData {
         }
         return instance;
     }
+    private ActionEvent event;
+    private Stage stage;
+    private Communicator communicator;
+    private String username;
+    private GUIView view;
 
+    /**
+     * This method is the getter of the private attribute event.
+     */
     public ActionEvent getEvent() {
         return event;
     }
+
+    /**
+     * This method is the setter of the private attribute event.
+     */
     public void setEvent(ActionEvent event) {
         this.event = event;
     }
 
+    /**
+     * This method is the getter of the private attribute stage.
+     */
     public Stage getStage() {
         return stage;
     }
 
+    /**
+     * This method is the setter of the private attribute stage.
+     */
     public void setStage(Stage stage) {
         this.stage = stage;
     }
 
+    /**
+     * This method is the setter of the private attribute communicator.
+     */
     public void setCommunicator(Communicator communicator){
         this.communicator = communicator;
     }
-    public void setUsername(String username){
-        this.username = username;
-    }
+
+    /**
+     * This method is the getter of the private attribute communicator.
+     */
     public Communicator getCommunicator(){
         return this.communicator;
     }
+
+    /**
+     * This method is the setter of the private attribute username.
+     */
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    /**
+     * This method is the getter of the private attribute username.
+     */
     public String getUsername(){
         return this.username;
     }
+
+    /**
+     * This method is the setter of the private attribute view.
+     */
     public void setView(GUIView view){
         this.view = view;
     }
+
+    /**
+     * This method is the getter of the private attribute username.
+     */
     public GUIView getView(){
         return this.view;
     }
